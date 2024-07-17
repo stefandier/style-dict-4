dc49f238da3deb990db65ebc1bd876d643c9c51e
stfn <stefan.dierdorf@gmail.com>
2024-07-17 14:45:32 +0200

    somewhat working but only using last sem. color file

--- build.mjs
+++ build.mjs
@@ -1,10 +1,26 @@
 
-import StyleDictionary from "style-dictionary";
+import { register } from '@tokens-studio/sd-transforms';
 import { getReferences, resolveReferences, usesReferences } from "style-dictionary/utils";
+import StyleDictionary from "style-dictionary";
 import JsonToTS from 'json-to-ts';
 
 const sd = new StyleDictionary("config.json");
+//const sd = new StyleDictionary();
 sd.log.verbosity = "verbose";
+
+//resolveReferences(sd.allTokens);
+
+// sd-transforms, 2nd parameter for options can be added
+// See docs: https://github.com/tokens-studio/sd-transforms
+register(sd, {
+    expand: {
+        composition: true,
+        typography: false,
+        border: false,
+        shadow: false,
+    },
+    excludeParentKeys: false,
+});
 
 await sd.cleanAllPlatforms();
 await sd.buildAllPlatforms();
